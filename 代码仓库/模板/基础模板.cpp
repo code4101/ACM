2732e0040931b5207155b5c8ffe560b188e62e54
code4101 <877362867@qq.com>
2016-07-27 09:49:39 +0800

    UVa12657 双向链表
    同时增加“基础模板1”

--- 代码仓库/模板/基础模板.cpp
+++ 代码仓库/模板/基础模板.cpp
@@ -19,17 +19,7 @@
 using namespace std;
 typedef long long LL;
 #define rep(i,b) for(int i=0; i<(b); ++i)
-#define foreach(i,a) for(__typeof((a).begin()) i=a.begin(); i!=(a).end(); ++i)
-
-// 任意整数的读入外挂 
-template <typename T> inline int read(T& x) {
-	int c, sign = 1; x = 0;
-	while (!(((c = getchar()) >= '0'&&c <= '9') || c == '-')) if (c == EOF) return c;
-	if (c == '-') sign = -1; else x = x * 10 + c - '0';
-	while (((c = getchar()) >= '0'&&c <= '9')) x = x * 10 + c - '0';
-	x *= sign;
-	return 1;
-}
+#define foreach(i,a) for(decltype((a).begin()) i=a.begin(); i!=(a).end(); ++i)
 
 int main() {
 	//ios::sync_with_stdio(false);
