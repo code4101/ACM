61b57e0c559ee28f78b8c4f771ebf9480f667efa
code4101 <877362867@qq.com>
2016-09-27 05:02:39 +0800

    ...

--- 忽略掉几个文件的改动.sh
+++ 忽略掉几个文件的改动.sh
@@ -1,4 +1,4 @@
-#忽略这两个文件在本地中的改动，不做提交。但仓库版本里仍然有这两个的空文件。
+#忽略这几个文件在本地中的改动，不做提交。但仓库版本里仍然有这两个的空文件。
 git update-index --assume-unchanged ACM/main.cpp
 git update-index --assume-unchanged wkDir/in.txt
 git update-index --assume-unchanged wkDir/out.txt
