80761911aefa1f1d667cd09a25a559e720f61851
code4101 <877362867@qq.com>
2017-03-28 21:45:14 +0800

    简化项目结构

--- ACM/main.cpp
+++ ACM/main.cpp
@@ -1,1 +1,34 @@
-﻿
+﻿#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long LL;
+#define rep(i,b) for(int i=0; i<(b); i++)
+#ifdef _MSC_VER
+#  define foreach(i,a) for(decltype((a).begin()) i=a.begin(); i!=(a).end(); ++i)
+#else
+#  define foreach(i,a) for(__typeof((a).begin()) i=a.begin(); i!=(a).end(); ++i)
+#endif
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+#endif
+
+	return 0;
+}
